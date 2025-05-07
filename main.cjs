5d7e2f1151e2d0e7581f49e3dc8acb288420e7a7
questaoresponse <139072788+questaoresponse@users.noreply.github.com>
2025-05-07 20:33:24 -0300

    update

--- main.cjs
+++ main.cjs
@@ -10,6 +10,12 @@ app.get("/getfile",(req,res)=>{
 app.listen(12345, (err)=>{
     console.log('live');
 });
+setInterval(()=>{
+    try {
+        axios.get("https://scrap-t8xu.onrender.com");
+    } catch (e){
+    }
+},10000);
 
 const auth = new GoogleAuth({
   keyFile: './firebase-service-account.json', // caminho para o .json
