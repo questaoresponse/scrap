cf9b915855d0ff14767aeef3003f27a43092d25c
questaoresponse <eneagonlosamigos@gmail.com>
2025-05-08 16:14:47 -0400

    update

--- main.cjs
+++ main.cjs
@@ -1,37 +1,45 @@
 const puppeteer = require('puppeteer');
 const fs = require("fs");
 const axios = require("axios");
-const { GoogleAuth } = require('google-auth-library');
 const admin = require("firebase-admin");
 const express = require("express");
 const app = express();
+app.use(express.json());
 app.get("/getfile",(req,res)=>{
     res.sendFile(__dirname + "/data.json");
+});
+app.get("/token",(req,res)=>{
+    res.sendFile(__dirname + "/scrap_web/index.html");
+    fs.readFile("tokens.json", "utf-8", (err, data) => {
+        const content = JSON.parse(data);
+        content.push(req.body.token);
+        fs.writeFile("tokens.json",JSON.stringify(content), (err) => {
+            console.log(err);
+        });
+    });
+});
+app.post("/token",(req,res)=>{
+    const body = req.body;
+    console.log(body);
 });
 app.listen(12345, (err)=>{
     console.log('live');
 });
 
 setInterval(()=>{
-    try {
-        axios.get("https://scrap-t8xu.onrender.com");
-    } catch (e){
-    }
+    axios.get("https://scrap-t8xu.onrender.com").then(r=>{}).catch(e=>{});
 },10000);
 
 const text = "eyJ0eXBlIjoic2VydmljZV9hY2NvdW50IiwicHJvamVjdF9pZCI6InNjcmFwLWVmNGQ5IiwicHJpdmF0ZV9rZXlfaWQiOiIyNDc0YTUwNmM2YmQzOGVkYjkyMzYyODQ2MDhlYzFkMDg3ZTY3NjEwIiwicHJpdmF0ZV9rZXkiOiItLS0tLUJFR0lOIFBSSVZBVEUgS0VZLS0tLS1cbk1JSUV2QUlCQURBTkJna3Foa2lHOXcwQkFRRUZBQVNDQktZd2dnU2lBZ0VBQW9JQkFRQ2ZMT3I4VHBpeDVDT3Jcbmx5VTVXYlMxNGdrNW9CS3R3V09raVBwTWJ1MlJYUThMQ1pkbGVuR0FOaWZodHF6QWk0YmM3VmI0K1pjVEJsdDFcblBBUmtLeU9YUDZidFJ4cnRGOVNjSDVlV0prcjZyWGR2cFdvaWFhZFlyU0Z0YmtxK25TNVZieVl4dkNEYW9Fd3lcbmpwMnQ1U3VBcGhwaTlsU2xXbmk3Wnl0dEF0dFpjckJKMERNUExJUWtwSVZUYi82RVQrQTlCU1pVOEw0YWFpYlBcbnJGdmhBNTJ4VHNTVG9UMElHTGtCZXVvVWtYVThzRkplVVo2WldCSjNuWVdGQzIvaFhTTUM5eUphbmFxTXM2VWNcblVpUHo3TEwyemJHalhzd05FazdVU3pGMlpUUUo4aHBDa0VzVThNclpLUTlYS3UrVlJPajNmY2NwV3hYM1pTeUxcbnZqSzRnSjlGQWdNQkFBRUNnZ0VBQ0FKSkVvZmIyL3VJSmU2VmMyVEZtRzgybXAwb3czQmJGS0RGdzBuRGoxQllcbjNPdlBIODZBM253YkZwckw1Mlk5MDJqbnV1QklEeTkvTTQ5aEF5OCtlN0k2c202azlxLy94Ums3MWRTNGFoT0hcblVtbEd4Vk4rbFpJa2YrNlFPVFlNYWtVTTdXeXpWSHpEU0xudlNod2hTN2hQMUpIallXeURiWjU3eTJEYzY0dXFcbjZkOFdkVS84YWNiT1NCMFRxcDZvbTBjNFNuR0s1QVkwWWp0anZ5anBDelN2QjlXOTNEVEVNN0lxMHJpZDFWRnBcbllTYVZERlhjSjEwWmtMNFg0aUV2UGtkOFJlZERvcVMxeFRTQ3pTMktRRFgzWi9pRFhaRFh0NG1MZEVEeS93TnFcblk1VFhsTHNBVWlCT3ZvMFFVWi9XZ2VZRVNFWnJaemJGWnducXpCNUY0UUtCZ1FEUnlPZFNQR1BjUGpOKzh1bm5cbkZqMmNxb2RPMjhleVIxd2JSQ0NjaThDRVFUaHJYMmNjYnVjOUtKY1V0TUJpOXBGQjVhbEV6blJjZGhNQmY4eGhcbk1TVFlxaHg5N0NVdEdrWVc0Vk5BVmdlV1RSSkFseDAzZkFTZTk1Y0ZaRENTSXFvVXJiR2VjaEd6R1o2YmhuakZcbnFLK0JWblRLR2huOWhpUGZISXBNZGNDM2RRS0JnUURDUGRWVDJNUVl3cXg5RERsQUdHQll4WGplSXgySHcwbWpcbkE5dTJjS2VvdmdQR2d4VW9lM3o5azlnYjgrakkxakV0T3ViOGhWLzNSNEpLdlFpcUltQlNKdlRESlFhRS9CWUhcbmdKSytZRmZTbElWbUd6VUVLUUhKYzdpMjVicCtiV2JqbmR1RWZsYUhraWRJeHg5N3ZJUUJJZy9ZV3Nic2M3blNcbk5FckdpMVVla1FLQmdGT2lBMDVPa3dVSHF5SHVGMyt2NXlvZzljWmphZzVSMG85d0xvd2FUc3FxOGFJa3E0ZXpcblV1NlgwdnRIUC9kMEV0NnhmelRXRlorUzF1cmZGd3ljT2ZOQ2hnOVJhRlM5eDdseFpPNGVIZkkya1pNOGEwSm1cblo3VTBRWG05d250UDVIeFA3cUtiR1NGTWttV3dTMCtKZTBaVndFT2Qra1NQQ0pmMjJjKzNsZmZOQW9HQWZtOUhcblErdXVRVHUra1NyNnZRamc5Z2VjRkdRaEhiVDFZcUhwUzBqcktVQitMcUkyaklKTktyWW9qcEZtcmd3V09JM0ZcbnV3dUtHdWkyRzdZT1JNaFprRUFCUkdEK2JHTzJaeTIrMXlMWko1NDdjdW8weVQ5MEJWQkpKT3hHUjNNbzdQSmNcbjZGSmdPcURjNTFlTjI3b3E1cDNvV0hsR1VMVkhlRmRIY1JOUzFiRUNnWUJ0RlFzWllhclBDc21TMWMzNThzLzFcbmtVYk9DaDJCS0FpaC9EZzU1R1V3a1NBaEcvc3F3bE5yQ0VVejJFYlFJZHl0aHk3bzNQd1VtVWEyVEphVVJpSEdcbkNiOUpLVS9VSG84OEROaXEvbURvK3NzSHZoNTBBdzR2S1E0SEdwMXFLZlcxcVdoc3VnMndaYWxVaDlzai83S0dcbnlKNUJ2Q1U5SmxyOEtLcWRzOWdPMkE9PVxuLS0tLS1FTkQgUFJJVkFURSBLRVktLS0tLVxuIiwiY2xpZW50X2VtYWlsIjoiZmlyZWJhc2UtYWRtaW5zZGstZmJzdmNAc2NyYXAtZWY0ZDkuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJjbGllbnRfaWQiOiIxMDgzMDI1NzI4MDU2OTA3MjkwODAiLCJhdXRoX3VyaSI6Imh0dHBzOi8vYWNjb3VudHMuZ29vZ2xlLmNvbS9vL29hdXRoMi9hdXRoIiwidG9rZW5fdXJpIjoiaHR0cHM6Ly9vYXV0aDIuZ29vZ2xlYXBpcy5jb20vdG9rZW4iLCJhdXRoX3Byb3ZpZGVyX3g1MDlfY2VydF91cmwiOiJodHRwczovL3d3dy5nb29nbGVhcGlzLmNvbS9vYXV0aDIvdjEvY2VydHMiLCJjbGllbnRfeDUwOV9jZXJ0X3VybCI6Imh0dHBzOi8vd3d3Lmdvb2dsZWFwaXMuY29tL3JvYm90L3YxL21ldGFkYXRhL3g1MDkvZmlyZWJhc2UtYWRtaW5zZGstZmJzdmMlNDBzY3JhcC1lZjRkOS5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVuaXZlcnNlX2RvbWFpbiI6Imdvb2dsZWFwaXMuY29tIn0=";
+
+const user_tokens = "e9p6KIj1QkimIwlBOzQtm8:APA91bE608K8NUeKnyXfLx08kqGDF654Qt5V62-Q4dgjPpN_Wt4zuVniR_QFy_KfRkK8kHVBGiEZmILHvm3lZO-tmU9ffdn6K7o78Jzz_tbeuwT3y2xgwKE";
+const username = "2025111ISINF0063";
+const password = "Eneagonlosamigos2*";
 admin.initializeApp({
     credential: admin.credential.cert(JSON.parse(Buffer.from(text,"base64").toString("utf-8")))
 });
-admin.messaging().send({token:"aiahihais",notification:{body:"",title:"iiaai"}});
-const auth = new GoogleAuth({
-    keyFile: "./firebase-service-account.json", // caminho para o .json
-    scopes: ['https://www.googleapis.com/auth/firebase.messaging'],
-});
+//admin.messaging().send({token:user_tokens,notification:{body:"",title:"iiaai"}});
 
-const user_tokens = JSON.stringify(["e9p6KIj1QkimIwlBOzQtm8:APA91bE608K8NUeKnyXfLx08kqGDF654Qt5V62-Q4dgjPpN_Wt4zuVniR_QFy_KfRkK8kHVBGiEZmILHvm3lZO-tmU9ffdn6K7o78Jzz_tbeuwT3y2xgwKE"]);
-const username = "2025111ISINF0063";
-const password = "Eneagonlosamigos2*";
 
 const names = [
     "Informática Básica",
@@ -68,24 +76,14 @@ material_nums.map(( material_num, index ) => inversed_material_nums[material_num
 
 async function sendNotification(notification, token){
     try {
-        await axios.post('https://fcm.googleapis.com/v1/projects/scrap-ef4d9/messages:send', notification, {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`,
-            },
-        });
+        admin.messaging().send(notification.message);
     } catch (error) {
         console.error('Erro ao enviar notificação:', error.response?.data || error.message);
     }
 };
-async function getAccessToken() {
-  const client = await auth.getClient();
-  const accessToken = await client.getAccessToken();
-  return accessToken.token;
-}
 
 (async () => {
-    const token = await getAccessToken();
+    const token = "";
     
     const browser = await puppeteer.launch({
         headless: true, // Executa com interface gráfica
